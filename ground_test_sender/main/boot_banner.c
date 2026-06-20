7442b3904bb5752f8140f2589069b32ac5a27d07
Klein-stack <sandeepsnathan08@gmail.com>
2026-06-20 13:45:09 +0530

    removed the update in bootbanner and added esp_init in main

--- ground_test_sender/main/boot_banner.c
+++ ground_test_sender/main/boot_banner.c
@@ -13,7 +13,7 @@
 
 static const char *TAG = "ESP_NOW";
 
-void boot_banner(uint8_t *mac_addr){
+void boot_banner(void){
 
     
     printf("======================\n");
@@ -40,16 +40,6 @@ void boot_banner(uint8_t *mac_addr){
         ESP_LOGI(TAG, "%02X : %02X : %02X : %02X : %02X : %02X" , mac_buffer[0], mac_buffer[1], mac_buffer[2], mac_buffer[3], mac_buffer[4],mac_buffer[5]);
         ESP_LOGI(TAG, "SYSTEM READY!!!");
     
-
-    if(mac_addr != NULL){
-        memcpy(mac_addr, mac_buffer, 6);
+    } else {
+        ESP_LOGI(TAG, "MAC ADDRESS RETRIEVAL UNSUCCESSFUL");
     }
- } else {
-        ESP_LOGE(TAG, "FAILED TO READ MAC ADDRESS!");
-        if(mac_addr != NULL){
-            memset(mac_addr, 0, 6);
-
-        }
-    }
-
-}
--- ground_test_sender/main/main.c
+++ ground_test_sender/main/main.c
@@ -8,12 +8,37 @@
 #include "freertos/task.h"
 #include "nvs_flash.h"
 #include "esp_now.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_random.h"
+#include "esp_netif.h"
 
 #include "boot_banner.h"
 
 #define TAG "ESP_NOW"
+#define ESPNOW_WIFI_MODE WIFI_MODE_STA
+#define CONFIG_ESPNOW_CHANNEL 1
+static uint8_t BROADCAST_MAC[ESP_NOW_ETH_ALEN] = {0xA0, 0x85, 0xE3, 0x0D, 0x84, 0x10}; //copied the MAC address from receiver side
 
-const uint8_t BROADCAST_MAC[ESP_NOW_ETH_ALEN];
+static void esp_now_init(void){
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(ESPNOW_WIFI_MODE));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
+
+
+}
+
+
+
+
+
+
+
 
 
 void app_main(void){
